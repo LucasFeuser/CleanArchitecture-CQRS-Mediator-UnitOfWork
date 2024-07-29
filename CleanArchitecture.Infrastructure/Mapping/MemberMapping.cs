a1074e2d919703d7a16ed6a1e9bfb3a10c111bb7
Lucas Feuser <l.feuser@nextage.com.br>
2024-07-29 20:43:26 -0300

    Fix Mapping

--- CleanArchitecture.Infrastructure/Mapping/MemberMapping.cs
+++ CleanArchitecture.Infrastructure/Mapping/MemberMapping.cs
@@ -19,9 +19,5 @@ public class MemberMapping : IEntityTypeConfiguration<Member>
         builder.Property(p => p.Salary).IsRequired();
         builder.Property(p => p.BirthDate).IsRequired();
         builder.Property(p => p.IsActive).IsRequired();
-        
-        builder.HasData(
-            new Member(1, "Lucas Feuser", "lucasvinchfeuser@gmail.com", "Sobre mim: Eu sou eu", DateTime.Now, 25, true, 5000)
-        );
     }
 }
